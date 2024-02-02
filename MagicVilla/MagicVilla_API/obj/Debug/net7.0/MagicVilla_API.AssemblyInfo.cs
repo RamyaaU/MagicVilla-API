6cdf688b78974755925850f4fdef152f4da9ce4e
Ramya <ramyayadav2016@gmail.com>
2024-02-03 00:00:03 +0530

    seeding villa table

--- MagicVilla/MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla/MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d153218c42086506ad356f2cba71cde0dcf60ac6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b69f43036056e72e433eb2a00cf5b59675929a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
