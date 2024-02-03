bbf6e02f4dc5bf0df167e42cdbe5a5157b51b693
Ramya <ramyayadav2016@gmail.com>
2024-02-03 12:04:55 +0530

    created separate dto classes for update and create

--- MagicVilla/MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla/MagicVilla_API/obj/Debug/net7.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdf688b78974755925850f4fdef152f4da9ce4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75293842e4e3bf13f2861a9c4e0482d652fe5b17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
